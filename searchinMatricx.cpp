0052b30cbedd9f045ff80f11e8416077d17d95aa
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-06-11 20:41:25 +0530

    Update searchinMatricx.cpp

--- searchinMatricx.cpp
+++ searchinMatricx.cpp
@@ -37,3 +37,27 @@ bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target
     
     return false;
 }
+
+//optimised approach - O(log(m*n))
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int low = 0;
+        int high = m*n - 1;
+        
+        while(low<=high) {
+            int mid = (low + high) / 2;
+            int r = mid / m;
+            int c = mid % m;
+            
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] > target) high = mid-1;
+            else low = mid + 1;
+        }
+        
+        return false;
+    }
+};
