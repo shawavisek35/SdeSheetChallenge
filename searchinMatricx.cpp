72217ecff2ddde7b55ebdfeaaf4723aa00eac7c1
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-06-11 13:00:25 +0530

    Update searchinMatricx.cpp

--- searchinMatricx.cpp
+++ searchinMatricx.cpp
@@ -13,3 +13,27 @@ bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target
     
     return false;
 }
+
+//optimised approach -> O(n+m)
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    //Binary search in 2D matrix
+    int i = 0;
+    int j = n-1;
+    
+    while(i < m && j >= 0) {
+        if(mat[i][j] == target) {
+            return true;
+        }
+        else if(mat[i][j] < target) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+    
+    return false;
+}
