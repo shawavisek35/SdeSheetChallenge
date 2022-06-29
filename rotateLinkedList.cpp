96b15f91a5e7b03c281bb01e08f029299294200a
Avisek Shaw <56290578+shawavisek35@users.noreply.github.com>
2022-06-29 08:38:43 +0530

    Update rotateLinkedList.cpp

--- rotateLinkedList.cpp
+++ rotateLinkedList.cpp
@@ -1,40 +1,40 @@
-#include <bits/stdc++.h> 
-/********************************
-
-    Following is the class structure of the Node class:
-    
-    class Node
-    {
-    	public:
-    	    int data;
-    	    Node *next;
-    	    Node(int data)
-    	    {
-    	        this->data = data;
-    	        this->next = NULL;
-    	    }
-    };
-
-********************************/
-
-Node *rotate(Node *head, int k) {
-     // Write your code here.
-    if(!k || !head || !head->next) return head;
-    Node *fast = head;
-    Node *slow = head;
-    while(k) {
-        k--;
-        fast = fast->next;
-        if(!fast) fast = head;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!k || !head || !head->next) return head;
+        int cnt = 0;
+        ListNode *temp = head;
+        while(temp) {
+            cnt ++;
+            temp = temp->next;
+        }
+        k = k%cnt;
+        if(!k) return head;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(k) {
+            k--;
+            fast = fast->next;
+            if(!fast) fast = head;
+        }
+        if(fast == head) return head;
+        while(fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode *newHead = slow->next;
+        fast->next = head;
+        slow->next = NULL;
+        return newHead;
     }
-    if(fast == head) return head;
-    while(fast->next) {
-        fast = fast->next;
-        slow = slow->next;
-    }
-    Node *newHead = slow->next;
-    fast->next = head;
-    slow->next = NULL;
-    return newHead;
-    
-}
+};
